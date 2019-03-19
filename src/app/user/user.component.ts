6f3d55901071db275b687ef7397ccbf267f68f4b
lujiewen <jevense@qq.com>
2019-03-19 21:07:01 +0800

    init

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -30,38 +30,38 @@ export class UserComponent implements OnInit {
 
   listOfData = [];
 
-  value: string[] = [ '0-0-0' ];
-  nodes = [ {
-    title   : 'Node1',
-    value   : '0-0',
-    key     : '0-0',
-    children: [ {
-      title : 'Child Node1',
-      value : '0-0-0',
-      key   : '0-0-0',
+  value: string[] = ['0-0-0'];
+  nodes = [{
+    title: 'Node1',
+    value: '0-0',
+    key: '0-0',
+    children: [{
+      title: 'Child Node1',
+      value: '0-0-0',
+      key: '0-0-0',
       isLeaf: true
-    } ]
+    }]
   }, {
-    title   : 'Node2',
-    value   : '0-1',
-    key     : '0-1',
-    children: [ {
-      title : 'Child Node3',
-      value : '0-1-0',
-      key   : '0-1-0',
+    title: 'Node2',
+    value: '0-1',
+    key: '0-1',
+    children: [{
+      title: 'Child Node3',
+      value: '0-1-0',
+      key: '0-1-0',
       isLeaf: true
     }, {
-      title : 'Child Node4',
-      value : '0-1-1',
-      key   : '0-1-1',
+      title: 'Child Node4',
+      value: '0-1-1',
+      key: '0-1-1',
       isLeaf: true
     }, {
-      title : 'Child Node5',
-      value : '0-1-2',
-      key   : '0-1-2',
+      title: 'Child Node5',
+      value: '0-1-2',
+      key: '0-1-2',
       isLeaf: true
-    } ]
-  } ];
+    }]
+  }];
 
   onChange($event: string[]): void {
     console.log($event);
